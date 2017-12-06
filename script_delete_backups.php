67337ff4d31ccd5d711a0f57465d56a53d7b0eac
Patricia Guzman <patricia.guzman@lcoral.com>
2017-12-05 17:39:20 -0800

    Updates to script that deletes old backups

--- script_delete_backups.php
+++ script_delete_backups.php
@@ -7,16 +7,15 @@ echo getcwd() . PHP_EOL;
 
 $weeks=3;
 $c = `ls -al`;
-echo $c .PHP_EOL;
+//echo $c .PHP_EOL;
 echo var_dump($c);
 
 
 
 foreach($files as $f){
-	if(filemtime($f) < time() - ){
+	if(filemtime($f) < time() - $weeks * (7 * 24 * 3600)){
 		unlink($f);
+		echo "Se han borrado backups antiguos" . PHP_EOL;
 	}
-	echo "Se han borrado backups antiguos" . PHP_EOL;
-
 }
 ?>
